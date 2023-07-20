0a8b43debeda6ab4cb66f9b4893ede449ee5c429
Bryan Vernon <bryan.vernon9@icloud.com>
2023-07-19 21:09:36 -0400

    Update rock-paper-scissors.py
    Much Closer

--- 2022/Day-2/rock-paper-scissors.py
+++ 2022/Day-2/rock-paper-scissors.py
@@ -1,34 +1,28 @@
-plays =[]
-final = 0
-playFile = ""
-
 def scoremaker(playFile):
-     
-    scores=[]
+    final = 0
+    scores = []
     tmp = 0
     with open(playFile, 'r') as file:
         for line in file:
             line = line.strip()
-            if line == 'A Y':
+            if line == 'AY':
                 tmp = 8
-            elif line ==' B X':
+            elif line == 'BX':
                 tmp = 1
-            elif line == 'C Y':
+            elif line == 'CY':
                 tmp = 2
-            elif line == 'C X':
+            elif line == 'CX':
                 tmp = 7
-            elif line == 'B Z':
+            elif line == 'BZ':
                 tmp = 9
-            elif line == 'A Z':
+            elif line == 'AZ':
                 tmp = 3
             else:
-                tmp = 6
-        scores.append(tmp)
+                tmp = 3
+            scores.append(tmp)
+            final += tmp
+    print(final)
     return scores
-plays = scoremaker('playbyplay.txt')
 
-for i in range(0, len(arr)):    
-   final = final + arr[i];  
-
-print(final)
-            
+playFile = "K:\Development Folder\Advent of code 2022\playbyplay.txt"
+plays = scoremaker(playFile)
